81c4442924997e6752f07635aa3d7799664620e5
Fkarau <30944677+Karau1218@users.noreply.github.com>
2026-03-05 10:42:13 -0800

    changed routes code

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -1,26 +1,13 @@
 import express from "express";
 import authCtl from "../controllers/auth.controller.js";
-import pageCtl from "../controllers/page.controller.js";
-import { hasRole } from "../controllers/auth.controller.js";
-import { logout } from "../controllers/auth.controller.js";
+import pageController from "../controllers/page.controller.js";
+import { isLoggedIn,hasRole, logout } from "../controllers/auth.controller.js";
+// import {  } from "../controllers/auth.controller.js";
 
 const router = express.Router();
-const isLoggedIn = (req, res, next) => {
-    if (req.session.user) {
-        return next();
-    } else {
-        return res.redirect("/login?errors=Please log in first");
-    }
-};
-const isAdmin = (req, res, next) => {
-    if (req.session.user && req.session.user.role === 'admin') {
-        return next(); 
-    } else {
-        return res.redirect("/dashboard?errors=Admins only.");
-    }
-};
 
-router.get("/", pageCtl.homePage);
+
+router.get("/", pageController.homePage);
 
 router.get("/login", authCtl.loginPage);
 router.post("/login", authCtl.login);
@@ -28,18 +15,12 @@ router.post("/login", authCtl.login);
 router.get("/register", authCtl.registerPage);
 router.post("/register", authCtl.register);
 
+//requires a logged in user
+router.get("/dashboard", isLoggedIn, pageController.dashboardPage);
+//requires an admin logged in
+router.get("/admin", isLoggedIn, hasRole("admin"), pageController.adminPage);
 
-router.get("/dashboard", isLoggedIn, (req, res) => {
-    console.log("Current session user:", req.session.user);
-    res.render("dashboard", { 
-        title: "Dashboard", 
-        user: req.session.user 
-    });
-});
-
-
-
-router.get("/admin", isLoggedIn, isAdmin, pageCtl.adminPage);
+// router.get("/admin", isLoggedIn, isAdmin, pageCtl.adminPage);
 router.get("/logout", logout);
 
 export default router;
